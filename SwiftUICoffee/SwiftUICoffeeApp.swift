11d45344e36b202b4dc5d8611588fa59e71e7f9c
abhrp <abhiroop.patel@gmail.com>
2020-10-01 18:47:38 +0530

    Created new project

---
+++ SwiftUICoffee/SwiftUICoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoffeeApp.swift
+//  SwiftUICoffee
+//
+//  Created by Abhiroop Patel on 01/10/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
