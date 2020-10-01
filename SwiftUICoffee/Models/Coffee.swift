0e0513bd5c64a98ea2300c48566f620cf573812e
abhrp <abhiroop.patel@gmail.com>
2020-10-01 18:55:30 +0530

    Added model and image assets

---
+++ SwiftUICoffee/Models/Coffee.swift
@@ -0,0 +1,24 @@
+//
+//  Coffee.swift
+//  SwiftUICoffee
+//
+//  Created by Abhiroop Patel on 01/10/20.
+//
+
+import Foundation
+
+struct Coffee: Decodable {
+    let name: String
+    let imageUrl: String
+    let price: Double
+}
+
+extension Coffee {
+    static func all() -> [Coffee] {
+        return [
+            Coffee(name: "Cappuccino", imageUrl: "Cappucinno", price: 2.5),
+            Coffee(name: "Espresso", imageUrl: "Espresso", price: 2.0),
+            Coffee(name: "Regular", imageUrl: "Regular", price: 1.0)
+        ]
+    }
+}
