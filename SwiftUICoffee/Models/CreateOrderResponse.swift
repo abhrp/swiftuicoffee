68dcc889d158a7e27da48c71c028be0a1ba3e709
abhrp <abhiroop.patel@gmail.com>
2020-10-01 22:25:38 +0530

    Added func for creating order

---
+++ SwiftUICoffee/Models/CreateOrderResponse.swift
@@ -0,0 +1,12 @@
+//
+//  CreateOrderResponse.swift
+//  SwiftUICoffee
+//
+//  Created by Abhiroop Patel on 01/10/20.
+//
+
+import Foundation
+
+struct CreateOrderResponse: Decodable {
+    let success: Bool
+}
